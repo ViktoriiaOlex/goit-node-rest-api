782bf4809210f8cbdd5276bbc62b7c49e4359c64
Victoriia Oleh <slavika86@gmail.com>
2024-05-20 13:48:12 +0300

    correct json

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -3,18 +3,11 @@ import Joi from "joi";
 export const createContactSchema = Joi.object({
   name: Joi.string().min(2).max(20).required(),
   email: Joi.string().email().required(),
-  phone: Joi.string()
-    .min(4)
-    .max(11)
-    .pattern(/^[0-9]+$/)
-    .required(),
+  phone: Joi.string().min(4).max(15).required(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(2).max(20),
   email: Joi.string().email().required(),
-  phone: Joi.string()
-    .min(4)
-    .max(11)
-    .pattern(/^[0-9]+$/),
+  phone: Joi.string().min(4).max(15),
 });
