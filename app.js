fa9042e5d0841fcdec1479061508cff638a9c112
Victoriia Oleh <slavika86@gmail.com>
2024-06-07 13:15:14 +0300

    create project

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
 import contactsRouter from "./routes/contactsRouter.js";
+import usersRouter from "./routes/usersRouter.js";
+import { checkToken } from "./middlewares/checkTokenMiddleware.js";
 
 const DB_KEY = process.env.DB_KEY;
 
@@ -13,7 +15,9 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", checkToken, contactsRouter);
+
+app.use("/api/users", usersRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -13,6 +13,7 @@ import {
   updateContactFavorite,
   updateContactSchema,
 } from "../schemas/contactsValidationSchema.js";
+import { createContactMiddleware } from "../middlewares/createContactMiddleware.js";
 
 const editValidation = validateBody(updateContactSchema);
 const editFavoriteValidation = validateBody(updateContactFavorite);
@@ -26,7 +27,12 @@ contactsRouter.get("/:id", getOneContact);
 
 contactsRouter.delete("/:id", deleteContact);
 
-contactsRouter.post("/", createValidation, createContact);
+contactsRouter.post(
+  "/",
+  createContactMiddleware,
+  createValidation,
+  createContact
+);
 
 contactsRouter.put("/:id", editValidation, updateContact);
 
